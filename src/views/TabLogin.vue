09a0f8b206fa45fb2e2341388eca841e6c215fc9
Cyclmobile <95096495+Cyclmobile@users.noreply.github.com>
2023-10-03 17:10:32 +0200

    Update TabLogin.vue

--- src/views/TabLogin.vue
+++ src/views/TabLogin.vue
@@ -1,9 +1,10 @@
 <template>
   <ion-page>
-    <ion-content class="login-background">
-      <swiper :options="swiperOptions">
+    <ion-content class="login-background" no-bounce>
+     <swiper :options="swiperOptions" ref="swiper">
         <!-- First Slide with Lottie Animation -->
         <swiper-slide>
+          
        <div class="slide-content">
     <iframe
       style="
@@ -17,12 +18,16 @@
         width: 100%;
         border: none;
       "
-      src="https://lottie.host/?file=d47df551-23bb-4c32-8b14-b41b1914756c/QON4d0Zl6J.json"
+      src="https://lottie.host/?file=b33cffe8-f41e-4eee-80d1-35a79f98361f/JqSyRZPxvc.lottie"
     ></iframe>
     <div class="text-container">
       <h1 class="slide-title">Recycling Station</h1>
       <p class="slide-text">Find nearby Recycling station</p>
     </div>
+<ion-button style="z-index: 1000;" class="skip-button" @click="skipToLogin">
+ <ion-icon aria-hidden="true" :icon="playForwardOutline" />
+    Skip to Login
+</ion-button>
   </div>
         </swiper-slide>
 
@@ -40,7 +45,7 @@
                 width: 100%;
                 border: none;
               "
-              src="https://lottie.host/?file=c1b5a1dd-7a76-4497-a586-0507f8e63622/eylqSqZXnx.json"
+              src="https://lottie.host/?file=3e495134-f97e-4560-a243-79762a4c9f9d/tTntzKkOdj.lottie"
             ></iframe>
              <div class="text-container"> 
                <h1 class="slide-title">Scan bottle</h1>
@@ -64,7 +69,7 @@
                 width: 100%;
                 border: none;
               "
-              src="https://lottie.host/?file=1841efff-2213-40ce-a8ca-53396fc889fb/BmMNxKGNS5.json"
+              src="https://lottie.host/?file=c764fae3-ae10-4471-8db6-2691f2c7380c/LNc3YKsxy5.lottie"
             ></iframe>
              <div class="text-container"> 
              <h1 class="slide-title">Recycling Station</h1>
@@ -90,59 +95,62 @@
                 width: 100%;
                 border: none;
               "
-              src="https://lottie.host/?file=b5c9094c-83b6-4a37-949f-333d73fe1034/LeMiwPeO9x.json"
+              src="https://lottie.host/?file=8ac28ac4-af28-4218-a95b-011641230fce/DVYHKLv4CL.lottie"
             ></iframe>
              <div class="text-container"> 
             <p class="slide-text">
                <h1 class="slide-title">Recycling Station</h1>
-              Drop you bottle and do not move your hand from camera to prevent
+              do not move your hand from camera to prevent
               losing points, wait for the signal then you done
             </p>
             </div>
           </div>
-        </swiper-slide>
-        <!-- Onboarding Slides -->
-        <!-- <swiper-slide v-for="path in imagePaths" :key="path">
-          <div class="slide-content">
-            <img :src="path" :alt="path" class="slide-image" />
-            <p class="slide-text">Text for {{ path }}</p>
-          </div>
-        </swiper-slide> -->
-
-        <!-- Login Slide -->
+              </swiper-slide>
         <swiper-slide>
-          <div class="login-container">
-            <ion-button expand="full" @click="googleLogin">
-              Login with Google
-            </ion-button>
-            <div class="login-container">
-              <ion-item>
-                <ion-input v-model="email" placeholder="Email" />
-              </ion-item>
-              <ion-item>
-                <ion-input
-                  v-model="password"
-                  placeholder="Password"
-                  type="password"
-                />
-              </ion-item>
-              <ion-button expand="full" @click="login">Login</ion-button>
-              <ion-text>
-                <a @click="goToSignup">Sign Up</a>
-              </ion-text>
-            </div>
-          </div>
-        </swiper-slide>
+
+  <div class="login-container">
+    <!-- Google Login -->
+    <ion-button expand="full" class="google-button" @click="googleLogin">
+          <ion-icon aria-hidden="true" :icon="logoGoogle"/>
+      Login with Google
+    </ion-button>
+    
+    <!-- Facebook Login -->
+    <!-- <ion-button expand="full" class="facebook-button" >
+          <ion-icon aria-hidden="true" :icon="logoFacebook" />
+      Login with Facebook
+    </ion-button> -->
+
+  <ion-button expand="full" class="apple-button" v-if="isIOS" @click="appleLogin">
+      <ion-icon aria-hidden="true" :icon="logoApple" />
+  Login with Apple
+</ion-button>
+
+    <div class="email-login-container">
+      <ion-item>
+        <ion-input v-model="email" placeholder="Email" />
+      </ion-item>
+      <ion-item>
+        <ion-input v-model="password" placeholder="Password" type="password" />
+      </ion-item>
+      <ion-button expand="full" @click="login">Login</ion-button>
+      <ion-text>
+        <a @click="goToSignup">Sign Up</a>
+      </ion-text>
+    </div>
+  </div>
+</swiper-slide>
+
       </swiper>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { ref, onMounted, onBeforeUnmount, nextTick } from "vue";
+import { isPlatform } from '@ionic/vue';
+import { ref, onMounted, onBeforeUnmount, nextTick,computed } from "vue";
 import { useRouter } from "vue-router";
 import Lottie from "lottie-vuejs"; // Import Lottie
-
 import {
   IonButton,
   IonContent,
@@ -151,7 +159,10 @@ import {
   IonPage,
   IonText,
   IonAlert,
+  IonIcon,
 } from "@ionic/vue";
+import { logoGoogle, logoFacebook,logoApple,arrowForward,playForwardOutline} from 'ionicons/icons';
+
 import {
   getAuth,
   signInWithEmailAndPassword,
@@ -159,7 +170,7 @@ import {
   indexedDBLocalPersistence,
   signInWithPopup,
   GoogleAuthProvider,
-  signOut,
+  OAuthProvider
 } from "firebase/auth";
 import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
@@ -167,15 +178,17 @@ import { Capacitor } from "@capacitor/core";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
 
+
+
 // Your firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3KjQPSVjw0PTwn1AkKdPLlW6yyom3_GE",
-  authDomain: "wifiyer.firebaseapp.com",
-  projectId: "wifiyer",
-  storageBucket: "wifiyer.appspot.com",
-  messagingSenderId: "409869290584",
-  appId: "1:409869290584:web:36bf46c2e3ecb098610e2d",
-  measurementId: "G-7GLD6P4N2G",
+  apiKey: "AIzaSyDpkOhaK9xqJoynNcq22EVSE44y5DebCSI",
+  authDomain: "cycl-ionic.firebaseapp.com",
+  projectId: "cycl-ionic",
+  storageBucket: "cycl-ionic.appspot.com",
+  messagingSenderId: "310990335294",
+  appId: "1:310990335294:web:c8cb7e850a93d94854d06f",
+  measurementId: "G-BSWJX4TVBF"
 };
 
 // Initialize Firebase directly
@@ -215,25 +228,90 @@ export default {
     const router = useRouter();
     const showAlert = ref(false);
     const provider = new GoogleAuthProvider();
+    const appleProvider = new OAuthProvider('apple.com');
+    
+
+
+     const isIOS = computed(() => {
+      return isPlatform('ios');
+    });
+
+    
+     const goToSignup = () => {
+      router.replace('/tabs/signup');
+    };
+
+     const currentSlideIndex = ref(0);
+const swiper = ref(null); // create a ref to hold the swiper instance
+
+
 
     // Lottie options
     const lottieOptions = {
       path: "https://lottie.host/?file=d47df551-23bb-4c32-8b14-b41b1914756c/QON4d0Zl6J.json", // path to the Lottie JSON file
       loop: true, // whether animation should loop
-      autoplay: true, // whether animation should autoplay
+      autoplay: false, // whether animation should autoplay
     };
 
     const googleLogin = async () => {
-      try {
-        const userCredential = await signInWithPopup(auth, provider);
-        // The signed-in user info.
-        const user = userCredential.user;
-        console.log("User successfully logged in with Google:", user);
-        router.push("tab1");
-      } catch (error) {
-        console.error("Error logging in with Google:", error);
-      }
-    };
+  try {
+    const userCredential = await signInWithPopup(auth, provider);
+    const user = userCredential.user;
+    console.log("User successfully logged in with Google:", user);
+     const tabBar = document.querySelector("ion-tab-bar");
+    if (tabBar) {
+      tabBar.style.display = "flex";
+    }
+await checkAndStoreUser(user);
+
+  } catch (error) {
+    console.error("Error logging in with Google:", error);
+  }
+};
+
+const checkAndStoreUser = async (user) => {
+  const userRef = doc(db, "users", user.uid);
+  const docSnap = await getDoc(userRef);
+    
+  if (!docSnap.exists()) {
+    await setDoc(userRef, {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL
+    });
+  }
+};
+
+
+
+
+
+ const appleLogin = async () => {
+  try {
+    const userCredential = await signInWithPopup(auth, appleProvider);
+    const user = userCredential.user;
+    console.log("User successfully logged in with Apple:", user);
+    
+    // Check and save user to Firestore if they don't exist
+    const userRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(userRef);
+    
+    if (!docSnap.exists()) {
+      await setDoc(userRef, {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+        photoURL: user.photoURL
+      });
+    }
+    
+    // Navigate to main page or any other required functionality
+  } catch (error) {
+    console.error("Error logging in with Apple:", error);
+  }
+};
+
 
     const preventLastSlideSwiping = async () => {
       const slides = document.querySelector("ion-slides");
@@ -251,39 +329,46 @@ export default {
       }
     };
 
-    // Get the currently logged-in user
-    const auth = getAuth(app);
+  
 
-    const user = auth.currentUser;
-    console.log(user);
-    // login user if user already signed in
-    // router.replace("tab1");
 
-    if (!user) {
-      // Redirect to login if the user is not logged in
-      router.replace("login"); // Adjust with your router setup
-    }
 
     onMounted(async () => {
       await nextTick(); // Wait for the DOM to be updated
+          swiper.value = document.querySelector('.swiper').swiper;
+
 
       const slides = document.querySelector("ion-slides");
       if (slides && slides.swiper) {
         slides.swiper.update();
-        console.log("Updated!!!")
       }
 
-      // const tabBar = document.querySelector("ion-tab-bar");
-      // if (tabBar) {
-      //   tabBar.style.display = "none";
-      // }
-    });
-
-    onBeforeUnmount(() => {
       const tabBar = document.querySelector("ion-tab-bar");
       if (tabBar) {
-        tabBar.style.display = "";
+        tabBar.style.display = "none";
       }
+
+        // Get the currently logged-in user
+    const auth = getAuth(app);
+     const user = auth.currentUser;
+  if (user) {
+    // Redirect to tab1 if the user is logged in
+    router.replace("tab1");
+  }
+  document.body.classList.add("hide-tab-bar");
+
+   
+    });
+
+     const skipToLogin = () => {
+    if (swiper.value) {
+      swiper.value.slideTo(4); // navigate to the 5th slide (0-index)
+    }
+  };
+
+    onBeforeUnmount(() => {
+      document.body.classList.remove("hide-tab-bar");
+
     });
 
     const signInUser = async (email: string, password: string) => {
@@ -334,14 +419,16 @@ export default {
         showAlert.value = true;
         console.log("User data fetch successful, redirecting...");
         router.replace("tab1");
+         const tabBar = document.querySelector("ion-tab-bar");
+    if (tabBar) {
+      tabBar.style.display = "flex";
+    }
       } catch (error) {
         console.error("Login failed", error);
       }
     };
 
-    const goToSignup = () => {
-      router.replace("/signup");
-    };
+  
 
     return {
       email,
@@ -351,25 +438,37 @@ export default {
       goToSignup,
       googleLogin,
       lottieOptions,
-      imagePaths: [
-        "/196.png",
-        "/216.png",
-        "/favicon.png",
-        "privacy.png",
-        "/terms.png",
-      ],
+      appleLogin,
       preventLastSlideSwiping,
       swiperOptions: {
         initialSlide: 0,
         speed: 400,
         direction: "horizontal",
+          touchAction: 'pan-x',
       },
+       logoGoogle,
+       logoApple,
+       logoFacebook,
+       skipToLogin,
+             isIOS,
+
     };
   },
 };
 </script>
 
 <style>
+body.hide-tab-bar ion-tab-bar {
+  display: none !important;
+}
+
+.skip-button {
+  position: absolute;
+  bottom: 20px;
+  right: 20px;
+  z-index: 1000;
+}
+
 .slide-title {
   color: #40b967; /* dark color for title */
   font-family: 'Roboto', sans-serif;
@@ -451,11 +550,14 @@ ion-button {
   --background: linear-gradient(135deg, #31b46f 0%, #28a965 100%);
   border-radius: 10px;
   box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);
-  transition: box-shadow 0.2s ease-in-out;
+    transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+
 }
 
 ion-button:hover {
   box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.15);
+    transform: scale(0.95);
+
 }
 
 ion-text a {
@@ -491,4 +593,55 @@ ion-content {
     font-size: 1.2em; /* Adjust font-size for smaller screens */
   }
 }
+body {
+  /* Prevent vertical scrolling */
+  overflow-y: hidden;
+}
+
+.button-icon {
+  height: 20px;
+  margin-right: 10px;
+}
+
+.google-button {
+  --background: #4285F4; /* Google's brand color */
+  color: white;
+}
+
+.facebook-button {
+  --background: #4267B2; /* Facebook's brand color */
+  color: white;
+}
+
+.apple-button {
+  --background: #000; /* Apple's brand color */
+  color: white;
+}
+
+/* Additional Styling */
+.login-container {
+  width: 80%;
+  border-radius: 20px;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  overflow: hidden;
+  background-color: #ffffff;
+  padding: 20px;
+}
+
+.email-login-container {
+  margin-top: 20px;
+}
+
+ion-button {
+  margin: 10px 0;
+  --padding-start: 15px;
+  --padding-end: 15px;
+}
+
+ion-item {
+  --background: #f0f0f0;
+}
+
+
 </style>
+
